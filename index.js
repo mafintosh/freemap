b5bb496266db2d31ce947e7bd75a3d797e2e9983
Mathias Buus <mathiasbuus@gmail.com>
2020-06-24 17:05:19 +0200

    fix name clash

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 module.exports = class FreeMap {
   constructor (reserved = 0) {
-    this.free = []
-    this.data = []
+    this._free = []
+    this._data = []
 
-    while (this.data.length < reserved) this.data.push(null)
+    while (this._data.length < reserved) this._data.push(null)
   }
 
   alloc () {
-    if (this.free.length) {
-      return this.free.pop()
+    if (this._free.length) {
+      return this._free.pop()
     }
-    this.data.push(null)
-    return this.data.length - 1
+    this._data.push(null)
+    return this._data.length - 1
   }
 
   add (data) {
     const id = this.alloc()
-    this.data[id] = data
+    this._data[id] = data
     return id
   }
 
   free (id) {
-    this.data[id] = null
-    this.free.push(id)
+    this._data[id] = null
+    this._free.push(id)
   }
 
   set (id, data) {
-    if (id === this.data.length) this.data.push(null)
-    if (id >= this.data.length) throw new Error('Invalid id')
-    this.data[id] = data
+    if (id === this._data.length) this._data.push(null)
+    if (id >= this._data.length) throw new Error('Invalid id')
+    this._data[id] = data
   }
 
   get (id) {
-    return this.data[id]
+    return this._data[id]
   }
 
   * [Symbol.iterator] () {
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i] !== null) yield [i, this.data[i]]
+    for (let i = 0; i < this._data.length; i++) {
+      if (this._data[i] !== null) yield [i, this._data[i]]
     }
   }
 }
